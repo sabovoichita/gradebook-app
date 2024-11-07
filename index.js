ce740197d6ff43b4c8a0deb5e356471c222d3f87
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-07 18:42:09 +0100

    1.Calculate Average

---
+++ index.js
@@ -0,0 +1,15 @@
+//average = sum of all scores / total number of scores
+function getAverage(scores) {
+  let sum = 0;
+  for (let i = 0; i < scores.length; i++) {
+    sum += scores[i];
+  }
+  return sum / scores.length;
+}
+
+console.log(
+  "Average: " + getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89])
+);
+console.log(
+  "Average: " + getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95])
+);
