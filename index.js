661cc1f9a4976ede704b7a2f5eda86dd43d8e5b3
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-08 09:18:26 +0100

    2. Converted the student score to a letter grade with function getGrade(score)

--- index.js
+++ index.js
@@ -17,6 +17,26 @@ function getAverage(scores) {
   return sum / scores.length;
 }
 
+function getGrade(score) {
+  if (score <= 59) {
+    return "F";
+  } else if (score <= 69) {
+    return "D";
+  } else if (score <= 79) {
+    return "C";
+  } else if (score <= 89) {
+    return "B";
+  } else if (score <= 99) {
+    return "A";
+  } else {
+    return "A++";
+  }
+}
+
+console.log("GetGrade: " + getGrade(96) + " for score of 96");
+console.log("GetGrade: " + getGrade(82) + " for score of 82");
+console.log("GetGrade: " + getGrade(56) + " for score of 56");
+
 console.log(
   "Average: " + getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89])
 );
